031952123e14ce00158a58a96c8c2eb758157b78
nick930116 <112918769+nick930116@users.noreply.github.com>
2022-10-18 11:53:26 +0800

    week07

---
+++ week07-4.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+    printf("瞷硂计:%d\n", n%10);
+    n = n / 10;
+}
